35edd038f2eadd0e49209de9befe1c91ec7e1709
Dima-shima <shifrin1970@gmail.com>
2026-05-07 12:45:24 +0300

    13 tests passed of 13

--- app/main.py
+++ app/main.py
@@ -8,26 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_list = []
-    wife_husband_dict = {}
+    people_list = ([Person(item["name"], item["age"]) for item in people])
     for item in people:
-        name = item["name"]
-        age = item["age"]
-        wife = item.get("wife")
-        husband = item.get("husband")
-        person = Person(name, age)
-        if "wife" in item.keys():
-            if wife:
-                person.wife = wife
-            wife_husband_dict[name] = ["wife", wife]
-        if "husband" in item.keys():
-            if husband:
-                person.husband = husband
-            wife_husband_dict[name] = ["husband", husband]
-        people_list.append(person)
-    for key, value in wife_husband_dict.items():
-        if value[0] == "wife" and value[1]:
-            Person.people[key].wife = Person.people[value[1]]
-        if value[0] == "husband" and value[1]:
-            Person.people[key].husband = Person.people[value[1]]
+        person = Person.people[item["name"]]
+        if item.get("wife"):
+            wife = item.get("wife")
+            person.wife = Person.people[wife]
+        if item.get("husband"):
+            husband = item.get("husband")
+            person.husband = Person.people[husband]
     return people_list
+
